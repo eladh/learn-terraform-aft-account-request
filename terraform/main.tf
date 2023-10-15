976f07dc7b3cab568e4518b7a2f12d52302c0bc5
eladhirsch <elad@terasky.com>
2023-10-15 11:41:58 +0300

    change params

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,7 @@ module "sandbox3" {
   }
 
   change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
+    change_requested_by = "HashiCorp Learn222"
     change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
   }
 
